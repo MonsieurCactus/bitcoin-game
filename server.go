0ffe37cdb1d133e95333be6196a5fa34fb11f385
monsieurcactus <john.mangual@gmail.com>
2015-10-25 07:53:07 -0400

    use blockchain api

--- server.go
+++ server.go
@@ -2,10 +2,33 @@ package main
 
 import "github.com/go-martini/martini"
 
+import "net/http"
+
+
+import "io/ioutil"
+
+
+
 func main() {
+
+
+
+
   m := martini.Classic()
   m.Get("/", func() string {
     return "Hello world!"
+  })
+  m.Get("/EUR", func() string {
+
+
+    resp, err := http.Get("https://blockchain.info/ticker")
+    robots, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+    	// handle error
+    }
+    defer resp.Body.Close()
+
+    return string(robots)
   })
   m.Run()
 }
