d8f5205cd965335d116b644394732958aa41cecd
monsieurcactus <john.mangual@gmail.com>
2015-10-25 08:41:57 -0400

    parse JSON with Go

--- server.go
+++ server.go
@@ -1,17 +1,45 @@
 package main
 
 import "github.com/go-martini/martini"
-
 import "net/http"
-
-
 import "io/ioutil"
+import "encoding/json"
+import "strconv"
 
+type Price struct {
+  Fifteen float64
+  Last float64
+  Buy float64
+  Sell float64
+  Symbol string
+}
 
+type Ticker struct
+{
+  USD  Price
+  ISK  Price
+  HKD  Price
+  TWD  Price
+  CHF  Price
+  EUR  Price
+  DKK  Price
+  CLP  Price
+  CAD  Price
+  CNY  Price
+  THB  Price
+  AUD  Price
+  SGD  Price
+  KRW  Price
+  JPY  Price
+  PLN  Price
+  GBP  Price
+  SEK  Price
+  NZD  Price
+  BRL  Price
+  RUB  Price
+}
 
 func main() {
-
-
 
 
   m := martini.Classic()
@@ -19,8 +47,6 @@ func main() {
     return "Hello world!"
   })
   m.Get("/EUR", func() string {
-
-
     resp, err := http.Get("https://blockchain.info/ticker")
     robots, err := ioutil.ReadAll(resp.Body)
     if err != nil {
@@ -28,7 +54,14 @@ func main() {
     }
     defer resp.Body.Close()
 
-    return string(robots)
+    var t Ticker
+
+    err = json.Unmarshal(robots, &t)
+    if err != nil {
+    	// handle error
+    }
+    //return string(robots) + " " + strconv.FormatFloat(t.EUR.Buy, 'E', -1, 64)
+    return "EUR: " + strconv.FormatFloat(t.EUR.Buy, 'E', -1, 64)
   })
   m.Run()
 }
